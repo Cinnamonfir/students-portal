88455a267624c2a2c1b421a54a4ba394252443dd
Hamidusman <abdulhamidusman218@gmail.com>
2023-10-06 20:16:48 +0100

    ...

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name=''),
+    path('delete/<int:pk>', views.delete_student, name='delete'),
+    path('add/', views.add, name='add'),
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
+from django.template import loader
+from .forms import Studentform
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+
 # Create your views here.
 
 def index(request):
     students = Student.objects.all()
-    context = {'students': students}
-    return render(request, 'index.html', context)
+    template = loader.get_template('index.html')
+    context = {'students': students}    
+    return HttpResponse(template.render(context, request))
+
+def delete_student(request, pk):
+    student = Student.objects.get(id=pk)
+    student.delete()
+    return redirect('')
+    
+def add(request):
+    form = Studentform(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('')
+    
+    context = {'form': form}
+    return render(request, 'add.html', context)
